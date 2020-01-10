67ecac13fbd054d910c7cebddac5042673e617db
hamedfathi <noeffect@outlook.com>
2020-01-10 19:01:46 +0330

    add(project)

---
+++ index.ts
@@ -0,0 +1,5 @@
+const arabicNumbers  = "٠١٢٣٤٥٦٧٨٩";
+const persianNumbers = "۰۱۲۳۴۵۶۷۸۹";
+const englishNumbers = "0123456789";
+const arabicChars = ["ي", "ك", "‍", "دِ", "بِ", "زِ", "ذِ", "ِشِ", "ِسِ", "ى","ة"];
+const persianChars = ["ی", "ک", "", "د", "ب", "ز", "ذ", "ش", "س", "ی","ه"];
